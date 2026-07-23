803952c75ba0ee273e894ff091d0cd87df683df4
Rinaldi <rinaldijames99@gmail.com>
2026-07-24 00:53:35 +0530

    Doubly Linked List - Get, Set, Insert

--- Doubly Linked Lists.py
+++ Doubly Linked Lists.py
@@ -118,7 +118,8 @@ class DoublyLinkedList:
         self.length -= 1
 
         #Return node that was popped
-        return temp.value
+        #return temp.value
+        return temp
     
 
     ###############################
@@ -158,6 +159,171 @@ class DoublyLinkedList:
 
         #Return true on completion
         return True
+    
+    
+    ###############################
+    #Pop First - Remove first item
+    # from the list and return what
+    # was popped
+    ###############################
+    def popFirst(self):
+
+        #For empty list return 'False'
+        if self.length <= 0:
+            return None
+        
+        #--------------#
+        
+        #Temporarily store current head node
+        temp = self.head
+
+        #For list of size 1
+        #Set head and tail as none
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+
+        #For lists greater than length one/'1'
+        # Make the 2nd Node as the head
+        # And disconnect 'next' and 'prev' pointers
+        else:
+
+            #Set head as the 2nd node
+            self.head = temp.next
+
+            #Disconnect prev pointer from new head
+            self.head.prev = None
+
+            #Disconnect next pointer from old head
+            temp.next = None
+
+        #Update length of the list
+        self.length -= 1
+        
+        #Return popped node
+        #return temp.value
+        return temp
+
+
+    ###############################
+    #Get - Fetch item from the list
+    # based on 'index' input
+    # And return the node
+    ###############################
+    def get(self,index):
+
+        #If index provided is out of bounds
+        if index<0 or index>=self.length:
+            return None
+        
+        #Temporarily store the current head
+        # value in 'temp'
+        temp = self.head
+
+        #Loop through all nodes
+        # if index is before first half
+        if index < (self.length)/2:
+            print("Get method used - First half of list.")
+            
+            for _ in range(index):
+                temp = temp.next
+
+        #Else parse through all nodes in
+        # the latter half starting from tail
+        else:
+            print("Get method used - Second half of list.")
+            temp = self.tail
+
+            #start from last node, which is usually n-1
+            # and subtract value by '1' to reach index
+            for _ in range(self.length-1, index, -1):
+                temp = temp.prev
+
+        #Return node after all steps complete
+        return temp
+    
+    ################################
+    #Set - Edit/change value at 
+    # specified index, and return 
+    # True or False
+    ################################
+    def set(self,index,value):
+
+        #Get and store current node 
+        # at that index
+        temp = self.get(index)
+
+        #If temp points to a node, 
+        # replace value
+        if temp:
+            temp.value = value
+            return True
+        
+        #Temp doesn't point to a node?
+        return False
+
+    ################################
+    #Insert - add value into specific
+    # position while keeping all 
+    # existing values - Return T or F
+    ################################
+    def insert(self,value,index):
+
+        #If provided index is out of bounds
+        if index<0 or index>self.length:
+            return False
+
+        #Use existing prepend function for
+        # index - 0
+        if index == 0:
+            return self.prepend(value)
+
+        #Use existing append function for
+        # last node, which is same as length
+        if index == self.length:
+            return self.append(value)
+
+        #For nodes except first and last. 
+        #---------------------------------------#
+        #---------------------------------------#
+        #Create new node, and store existing nodes 
+        # in 'before' and 'after'
+
+        #Create a new node with provided value
+        newNode = Node(value)
+
+        #Get value of node, right
+        # before index position.
+        # This will be on the left 
+        # of the new node.
+        before = self.get(index-1)
+
+        #Get 'current node'.
+        # This will move to
+        # the right of the new node
+        after = before.next
+        #---------------------------------------#
+        #Update the 'prev' and 'next' pointers
+        # of the three nodes
+
+        #New Node
+        newNode.prev = before
+        newNode.next = after
+
+        #Before node
+        before.next = newNode
+
+        #After node - Old node at that index
+        after.prev = newNode
+
+        #---------------------------------------#
+        #---------------------------------------#
+
+        #Update list length
+        self.length += 1
+
+        return True
+
 
     
 ###################################
@@ -218,12 +384,75 @@ print("\n#---------------#\n")
 ##################################
 #III. Test prepend
 ##################################
-print("\nPrepending List with value '0'\n")
+print("\nPrepending List with value '0'.\n")
 
 #Prepend value in the list
 myDoublyLinkedList.prepend(0)
 
 #Print result
+myDoublyLinkedList.print_list()
+
+print("\n#---------------#\n")
+
+
+##################################
+#IV. Pop first
+##################################
+print("Popping first value in the List.\n")
+
+#Pop first value in the list
+myDoublyLinkedList.popFirst()
+
+#Print result
+myDoublyLinkedList.print_list()
+
+print("\n#---------------#\n")
+
+
+##################################
+#V. Get Node
+##################################
+
+print("Get value from list based on index.\n")
+
+#Adding some values
+myDoublyLinkedList.prepend(0)
+myDoublyLinkedList.append(3)
+myDoublyLinkedList.append(4)
+myDoublyLinkedList.append(5)
+
+print(f"New list is now \n")
+myDoublyLinkedList.print_list()
+print("\n")
+
+#Get values at first and second half
+print(myDoublyLinkedList.get(2).value)
+print(myDoublyLinkedList.get(4).value)
+
+print("\n#---------------#\n")
+
+
+##################################
+#VI. Set Node
+##################################
+print("Set/edit value at provided 'index'.\n")
+
+#Set value at fifth position as '6'
+myDoublyLinkedList.set(5,6)
+
+myDoublyLinkedList.print_list()
+
+print("\n#---------------#\n")
+
+
+##################################
+#VII. Insert Node
+##################################
+print("Insert value at provided 'index'.\n")
+
+#Insert value at fifth position as '5'
+myDoublyLinkedList.insert(5,5)
+
 myDoublyLinkedList.print_list()
 
 print("\n#---------------#\n")
